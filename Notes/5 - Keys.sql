c9d86464113a14e792d2036cdcef3a3d7970798b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 23:55:31 +0000

    auto increment

--- Notes/5 - Keys.sql
+++ Notes/5 - Keys.sql
@@ -1,7 +1,7 @@
 -- Create a table with primaryKey
 
 CREATE TABLE Person (
-    id INTEGER PRIMARY KEY,             -- Primary Key, NOT NULL and UNIQUE for default            
+    id INTEGER PRIMARY KEY,                     -- Primary Key, NOT NULL and UNIQUE for default            
     name CHAR(30) DEFAULT "Unknown",
     adress VARCHAR(255),
     gender CHAR(1) DEFAULT '?',  
@@ -11,8 +11,11 @@ CREATE TABLE Person (
 -- Create a table with composed primaryKey
 
 CREATE TABLE Faculty (
-    id INTEGER AUTOINCREMENT,           -- Auto increment, based on last table tuple
+    id INTEGER AUTOINCREMENT,                   -- Auto increment, based on last table tuple
     name CHAR(30),
     city CHAR(10),
-    PRIMARY KEY (id, city)              -- (id+city) is a composed primary key
+    PRIMARY KEY (id, city)                      -- (id+city) is a composed primary key
 );
+
+INSERT INTO Faculty VALUES ("FEUP", "Porto");   -- id = 0
+INSERT INTO Faculty VALUES ("FCUP", "Porto");   -- id = 1
