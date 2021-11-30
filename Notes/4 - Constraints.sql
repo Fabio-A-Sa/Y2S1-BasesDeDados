f950a49be74678427b796d7508999a570deccce9
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 23:43:33 +0000

    More constrains

--- Notes/4 - Constraints.sql
+++ Notes/4 - Constraints.sql
@@ -4,6 +4,6 @@ CREATE TABLE Person (
     id INTEGER NOT NULL,                    -- In the insertion, if id == NULL, gives an error    
     name CHAR(30) DEFAULT "Unknown",        -- In the insertion, if name == NULL, name == "Unknown"
     adress VARCHAR(255),
-    gender CHAR(1),  
+    gender CHAR(1) DEFAULT '?',  
     birthday DATE,
 );
