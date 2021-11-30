fc8a21ef6cb1b58509267021c51ffecc1288ebac
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 23:46:45 +0000

    Multiple keys

--- Notes/5 - Keys.sql
+++ Notes/5 - Keys.sql
@@ -1,9 +1,16 @@
 -- Create a table with primaryKey
 
 CREATE TABLE Person (
-    id INTEGER NOT NULL,                  
+    id INTEGER PRIMARY KEY,            -- Primary Key, not Null              
     name CHAR(30) DEFAULT "Unknown",
     adress VARCHAR(255),
     gender CHAR(1) DEFAULT '?',  
     birthday DATE,
+);
+
+CREATE TABLE Faculty (
+    id INTEGER,               
+    name CHAR(30),
+    city CHAR(10),
+    PRIMARY KEY (id, city)              -- (id+city) is a composed primary key
 );
