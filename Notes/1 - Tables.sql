0795831eb01eceb09650aa04251d93126761973c
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 23:18:50 +0000

    More functions

--- Notes/1 - Tables.sql
+++ Notes/1 - Tables.sql
@@ -18,10 +18,20 @@ DROP TABLE IF EXISTS Person;    -- Remove if it's in current directory
 ALTER TABLE Person ADD country VARCHAR(255); -- Add atribute/collumn country
 ALTER TABLE Person REMOVE country;           -- Remove atribute/collumn country
 
+-- Insert data tuples
+
+INSERT INTO Person VALUES (123, "Fabio", "Oporto", 'M', "2002-07-10");
+INSERT INTO Person VALUES (4375, "Catarina", "Lisbon", 'F', "2002-11-07");
+INSERT INTO Person VALUES (123, "Joana", "Aveiro", 'F', "2002-07-09");
+
 -- To view all tables in current dataBase
 
 .tables
 
 -- To view schema of certain table
 
-.shema Person
+.schema Person
+
+-- To view collumn' name 
+
+.header on
