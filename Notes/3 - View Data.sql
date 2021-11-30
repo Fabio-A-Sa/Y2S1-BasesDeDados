77cafad18a9c8f3a2d3bd5e4b9844d83f0d53a2d
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 23:32:01 +0000

    Show data

---
+++ Notes/3 - View Data.sql
@@ -0,0 +1,4 @@
+-- To run a query
+
+SELECT * FROM Person;                                           -- Show all atributes and tuples from current table Person
+SELECT names, date('2021-11-30')-dateNascimento from Person;    -- Show name | age in current table Person
