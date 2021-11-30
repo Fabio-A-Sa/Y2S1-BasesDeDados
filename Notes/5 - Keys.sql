cf1517624d582b4ed4bd58affa445192ff0a4c71
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 23:53:24 +0000

    Primary keys

--- Notes/5 - Keys.sql
+++ Notes/5 - Keys.sql
@@ -1,15 +1,17 @@
 -- Create a table with primaryKey
 
 CREATE TABLE Person (
-    id INTEGER PRIMARY KEY,             -- Primary Key, not Null              
+    id INTEGER PRIMARY KEY,             -- Primary Key, NOT NULL and UNIQUE for default            
     name CHAR(30) DEFAULT "Unknown",
     adress VARCHAR(255),
     gender CHAR(1) DEFAULT '?',  
     birthday DATE,
 );
 
+-- Create a table with composed primaryKey
+
 CREATE TABLE Faculty (
-    id INTEGER,               
+    id INTEGER AUTOINCREMENT,           -- Auto increment, based on last table tuple
     name CHAR(30),
     city CHAR(10),
     PRIMARY KEY (id, city)              -- (id+city) is a composed primary key
