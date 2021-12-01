05e4bd48383e73326fcffdb4d4f4dded0aa1d620
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-01 01:18:27 +0000

    Drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -142,3 +142,31 @@ INSERT INTO Faculty (id, name, city) VALUES (1, "IPCA", "Braga")                
 
 -- Note
 -- If exists an integer ID, ID is an alias of ROWID (implicit primary key)
+
+-- Create tables
+
+CREATE TABLE Student (
+    sID INTEGER PRIMARY KEY,
+    sName CHAR(10),
+    grade FLOAT,
+    college CHAR(30)
+);
+
+CREATE TABLE College (
+    cName CHAR(10) PRIMARY KEY,
+    city CHAR(10)
+);
+
+CREATE TABLE Apply (
+    sID INTEGER REFERENCES Student(sID) ON UPDATE CASCADE ON DELETE SET NULL,            -- Student's primary key
+    cName CHAR(10) REFERENCES College(cName) ON UPDATE CASCADE ON DELETE SET NULL,       -- College's primary key
+    PRIMARY KEY (sID, cName)                        -- Aplly is a primary key too
+)
+
+-- Note
+-- if Student has (sID, sName) for Primary Key, Apply will have
+-- FOREING KEY (sID, sName) REFERENCES Student(sID, sName);
+
+-- Note
+-- if Apply's sID is updated, Student will be updated too
+-- if Apply's cName is deleted, College will be set to NULL
