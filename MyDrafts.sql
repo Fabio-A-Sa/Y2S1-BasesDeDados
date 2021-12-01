04577e91ea10d6352bfca3adbb4b3d58fb643e4d
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-01 01:17:55 +0000

    Drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -60,3 +60,8 @@ ALTER TABLE Person ADD phone CHAR(14) DEFAULT "+351999999999";  -- for default
 
 -- Note:
 -- The default default value of SQLite is NULL
+
+-- To run a query
+
+SELECT * FROM Person;                                           -- Show all atributes and tuples from current table Person
+SELECT names, date('2021-11-30')-dateNascimento from Person;    -- Show name | age in current table Person
