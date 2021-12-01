1f3cac81b866755a2bba24d8410a85a286d0b56b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-01 01:18:08 +0000

    Drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -65,3 +65,50 @@ ALTER TABLE Person ADD phone CHAR(14) DEFAULT "+351999999999";  -- for default
 
 SELECT * FROM Person;                                           -- Show all atributes and tuples from current table Person
 SELECT names, date('2021-11-30')-dateNascimento from Person;    -- Show name | age in current table Person
+
+-- Create a table with constraints
+
+CREATE TABLE Person (
+    id INTEGER NOT NULL,                    -- In the insertion, if id == NULL, gives an error    
+    name CHAR(30) DEFAULT "Unknown",        -- In the insertion, if name == NULL, name == "Unknown"
+    adress VARCHAR(255) UNIQUE,             -- Allows unique values in tuples, and allows NULL values
+    gender CHAR(1) DEFAULT '?',  
+    birthday DATE,
+    phone VARCHAR(14) UNIQUE
+);
+
+-- or
+
+CREATE TABLE Person (
+    id INTEGER NOT NULL,                    -- In the insertion, if id == NULL, gives an error    
+    name CHAR(30) DEFAULT "Unknown",        -- In the insertion, if name == NULL, name == "Unknown"
+    adress VARCHAR(255),           
+    gender CHAR(1) DEFAULT '?',  
+    birthday DATE,
+    phone VARCHAR(14),
+    UNIQUE (adress, phone)                  -- Allows unique subTuples and NULL
+    UNIQUE (name, birthdate)                -- Allows unique subTuples and NULL
+);
+
+-- Check constraints
+
+CREATE TABLE Faculty (
+    id INTEGER AUTOINCREMENT,
+    name VARCHAR(10),
+    city CHAR(100),
+    CHECK (name <> 'ISEP' AND city == 'COIMBRA')
+)
+
+INSERT INTO Faculty values ('ISEP', 'PORTO')            -- Gives an error, check violation
+INSERT INTO Faculty values ('FEUP', 'COIMBRA')          -- Correct
+
+-- Include constraints name -> Good practise
+
+CREATE TABLE Faculty (
+    id INTEGER AUTOINCREMENT,
+    name VARCHAR(10) CONSTRAINT NameNotNull NOT NULL,   -- If gives an error, show NameNotNull message
+    city CHAR(100) CONSTRAINT CityNotNull NOT NULL,     -- If gives an error, show CityNotNull message
+    CHECK (name <> 'ISEP' AND city == 'COIMBRA')
+    CONSTRAINT FacultyPKError PRIMARY KEY (id)          -- If gives an error, show FacultyPKError message
+);
+
