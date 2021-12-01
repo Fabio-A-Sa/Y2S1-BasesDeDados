0a6ca7712c25a891a4e4208bb06352b5c2ad28ae
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-01 01:17:34 +0000

    Drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -45,3 +45,18 @@ INSERT INTO Person VALUES (123, "Joana", "Aveiro", 'F', "2002-07-09");
 -- To enable Foreing Keys
 
 PRAGMA foreign_keys = ON;
+
+-- Create a table with default values, if insertion NULL
+
+CREATE TABLE Person (
+    id INTEGER DEFAULT -1,                                      -- -1, error  
+    name CHAR(30) DEFAULT "UNKNOWN",                            -- for default, Unknown 
+    adress VARCHAR(255) DEFAULT "Oporto",                       -- for default, Oporto
+    gender CHAR(1) DEFAULT 'X',                                 -- for default, X
+    birthday DATE DEFAULT now,                                  -- for default, date.now
+);
+
+ALTER TABLE Person ADD phone CHAR(14) DEFAULT "+351999999999";  -- for default
+
+-- Note:
+-- The default default value of SQLite is NULL
