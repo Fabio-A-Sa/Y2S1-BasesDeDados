d885ef6cef80bddb6d6f830db179556a0d1b38d0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-01 00:13:23 +0000

    Drafts

--- Notes/4 - Constraints.sql
+++ Notes/4 - Constraints.sql
@@ -3,7 +3,20 @@
 CREATE TABLE Person (
     id INTEGER NOT NULL,                    -- In the insertion, if id == NULL, gives an error    
     name CHAR(30) DEFAULT "Unknown",        -- In the insertion, if name == NULL, name == "Unknown"
-    adress VARCHAR(255),
+    adress VARCHAR(255) UNIQUE,             -- Allows unique values in tuples, and allows NULL values
     gender CHAR(1) DEFAULT '?',  
     birthday DATE,
+    phone VARCHAR(14) UNIQUE
+);
+
+-- or
+
+CREATE TABLE Person (
+    id INTEGER NOT NULL,                    -- In the insertion, if id == NULL, gives an error    
+    name CHAR(30) DEFAULT "Unknown",        -- In the insertion, if name == NULL, name == "Unknown"
+    adress VARCHAR(255),           
+    gender CHAR(1) DEFAULT '?',  
+    birthday DATE,
+    phone VARCHAR(14),
+    UNIQUE (adress, phone)                  -- Allows unique values in tuples, and allows NULL values
 );
