b3dc37bb7f0b3279b83a3f641ee7cd7e62466f1f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-03 17:03:31 +0000

    From 3 column

--- Notes/3 - View Data.sql
+++ Notes/3 - View Data.sql
@@ -1,4 +1,4 @@
 -- To run a query
 
 SELECT * FROM Person;                                           -- Show all atributes and tuples from current table Person
-SELECT names, date('2021-11-30')-dateNascimento from Person;    -- Show name | age in current table Person
+SELECT name, date('2021-11-30')-birthday from Person;    -- Show name | age in current table Person
