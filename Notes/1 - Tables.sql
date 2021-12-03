700fb45313975783ee217ed18cffad6ac9d82118
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-03 16:51:52 +0000

    Drafts

--- Notes/1 - Tables.sql
+++ Notes/1 - Tables.sql
@@ -1,14 +1,14 @@
 -- Create a table
 
-PRAGMA foreign_keys=OFF;
+PRAGMA foreign_keys = ON;
 BEGIN TRANSACTION;
 
 CREATE TABLE Person (
     id INTEGER,            -- Integer type
     name CHAR(30),         -- Immutable char array
-    adress VARCHAR(255),   -- Muttable char array
+    address CHAR(255),   -- Muttable char array
     gender CHAR(1),        -- Char value
-    birthday DATE,         -- String text, in format "YYYY-MM-DD"
+    birthday DATE          -- String text, in format "YYYY-MM-DD"
 );
 
 -- Remove a table
