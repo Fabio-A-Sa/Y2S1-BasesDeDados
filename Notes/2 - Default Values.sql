165591056be7cb7849834a00a2b8f529577414a6
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-03 17:09:31 +0000

    Modifications

--- Notes/2 - Default Values.sql
+++ Notes/2 - Default Values.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS Person;
 CREATE TABLE Person (
     id INTEGER DEFAULT -1,                                      -- -1, error  
     name CHAR(30) DEFAULT "UNKNOWN",                            -- for default, Unknown 
-    adress VARCHAR(255) DEFAULT "Oporto",                       -- for default, Oporto
+    address VARCHAR(255) DEFAULT "Oporto",                       -- for default, Oporto
     gender CHAR(1) DEFAULT 'X',                                 -- for default, X
-    birthday DATE DEFAULT now,                                  -- for default, date.now
+    birthday DATE DEFAULT '2021-11-30',                                  -- for default, date.now
 );
 
 ALTER TABLE Person ADD phone CHAR(14) DEFAULT "+351999999999";  -- for default
