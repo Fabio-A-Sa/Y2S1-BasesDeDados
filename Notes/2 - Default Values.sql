9c2e642f65e03f534b394af4107ea9540fb665cb
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-03 17:15:57 +0000

    New file

--- Notes/2 - Default Values.sql
+++ Notes/2 - Default Values.sql
@@ -3,11 +3,11 @@
 DROP TABLE IF EXISTS Person;
 
 CREATE TABLE Person (
-    id INTEGER,                                      -- -1, error  
-    name CHAR(10) DEFAULT 'UNKNOWN',                            -- for default, Unknown 
-    address CHAR(255) DEFAULT 'Oporto',                       -- for default, Oporto
-    gender CHAR(1) DEFAULT 'X',                                 -- for default, X
-    birthday DATE DEFAULT '2021-11-30',                                  -- for default, date.now
+    id INTEGER,                                  
+    name CHAR(10) DEFAULT 'UNKNOWN',                 -- for default, Unknown 
+    address CHAR(255) DEFAULT 'Oporto',              -- for default, Oporto
+    gender CHAR(1) DEFAULT 'X',                      -- for default, X
+    birthday DATE DEFAULT '2021-11-30',              -- for default, date.now
 );
 
 ALTER TABLE Person ADD phone CHAR(14) DEFAULT "+351999999999";  -- for default
