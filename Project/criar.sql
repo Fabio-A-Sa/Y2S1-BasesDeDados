e1377aee4c1940d28ea171a59fb492ffb7f255d7
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 17:40:01 +0000

    Something

--- Project/criar.sql
+++ Project/criar.sql
@@ -1,4 +1,5 @@
--- Eliminar as tabelas, se existirem
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS Pessoa;
 DROP TABLE IF EXISTS Armazem;
@@ -16,8 +17,6 @@ DROP TABLE IF EXISTS Utensilio;
 DROP TABLE IF EXISTS Vinho;
 DROP TABLE IF EXISTS Compra;
 
--- Criar as novas tabelas, com restrições
-
 CREATE TABLE Pessoa (
     idPessoa INTEGER PRIMARY KEY,
     nome CHAR(30) CONSTRAINT ErroPessoaNome NOT NULL,
@@ -27,18 +26,31 @@ CREATE TABLE Pessoa (
     nif INTEGER CONSTRAINT ErroPessoaNif NULL  
 );
 
+CREATE TABLE Funcionario (
+    idPessoa INTEGER CONSTRAINT ErroFuncionarioIdPessoa PRIMARY KEY REFERENCES Pessoa (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
+    salario INTEGER CONSTRAINT ErroFuncionarioSalario CHECK (salario >= 0)
+);
+
+CREATE TABLE Gerente (
+    idFuncionario CONSTRAINT ErroGerenteIdFuncionario PRIMARY KEY REFERENCES Funcionario (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
+    anosServico CONSTRAINT ErroGerenteIdAnosServico CHECK (anosServico > 4)
+);
+
+CREATE TABLE NaoGerente (
+    idFuncionario CONSTRAINT ErroNaoGerenteIdFuncionario PRIMARY KEY REFERENCES Funcionario (idFuncionario) ON UPDATE CASCADE ON DELETE CASCADE,
+    funcao TEXT
+);
+
+CREATE TABLE Cliente (
+    idPessoa INTEGER CONSTRAINT ErroClienteIdPessoa PRIMARY KEY REFERENCES Pessoa (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
+    maioridade BOOL CONSTRAINT ErroClienteMaioridade NOT NULL
+);
+
 CREATE TABLE Armazem (
     idArmazem INTEGER PRIMARY KEY,
     telefone INTEGER CONSTRAINT ErroArmazemTelefone NOT NULL,
     local TEXT CONSTRAINT ErroArmazemLocal NOT NULL,
     UNIQUE(telefone, local)
-);
-
-CREATE TABLE Stock (
-    idArmazem INTEGER CONSTRAINT ErroStockIdArmazem REFERENCES Armazem (idArmazem) ON DELETE CASCADE ON UPDATE CASCADE,
-    idProduto INTEGER CONSTRAINT ErroStockIdProduto REFERENCES Produto (idProduto) ON DELETE CASCADE ON UPDATE CASCADE,
-    quantidade INTEGER CONSTRAINT ErroStockQuantidade CHECK (quantidade > 0),
-    PRIMARY KEY (idArmazem, idProduto)
 );
 
 CREATE TABLE Horario (
@@ -56,48 +68,14 @@ CREATE TABLE Horario (
 );
 
 CREATE TABLE HorarioFuncionario (
-    idFuncionario INTEGER CONSTRAINT ErroHorarioFuncionarioIdFuncionario REFERENCES Funcionario (idFuncionario) ON UPDATE CASCADE ON DELETE CASCADE,
+    idFuncionario INTEGER CONSTRAINT ErroHorarioFuncionarioIdFuncionario REFERENCES Funcionario (idPessoa) ON UPDATE CASCADE ON DELETE CASCADE,
     idHorario INTEGER CONSTRAINT ErroHorarioFuncionarioIdHorario REFERENCES Horario (idHorario) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (idFuncionario, idHorario)
-);
-
-CREATE TABLE Funcionario (
-    idPessoa INTEGER CONSTRAINT ErroFuncionarioIdPessoa PRIMARY KEY REFERENCES Pessoa (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
-    salario INTEGER CONSTRAINT ErroFuncionarioSalario CHECK (salario >= 0)
-);
-
-CREATE TABLE Cliente (
-    idPessoa INTEGER CONSTRAINT ErroClienteIdPessoa PRIMARY KEY REFERENCES Pessoa (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
-    maioridade BOOL CONSTRAINT ErroClienteMaioridade NOT NULL
 );
 
 CREATE TABLE Produto (
     idProduto INTEGER PRIMARY KEY,
     preco INTEGER CONSTRAINT ErroProdutoPreco CHECK (preco >= 0)
-);
-
-CREATE TABLE Gerente (
-    idFuncionario CONSTRAINT ErroGerenteIdFuncionario PRIMARY KEY REFERENCES Funcionario (idFuncionario) ON DELETE CASCADE ON UPDATE CASCADE,
-    anosServico CONSTRAINT ErroGerenteIdAnosServico CHECK (anosServico > 4)
-);
-
-CREATE TABLE NaoGerente (
-    idFuncionario CONSTRAINT ErroNaoGerenteIdFuncionario PRIMARY KEY REFERENCES Funcionario (idFuncionario) ON UPDATE CASCADE ON DELETE CASCADE,
-    funcao TEXT
-);
-
-CREATE TABLE Cartao (
-    idCartao INTEGER PRIMARY KEY,
-    dataAdesao DATE CONSTRAINT ErroCartaoDataAdesao NOT NULL,
-    saldo INTEGER CONSTRAINT ErroCartaoSaldo CHECK (saldo>=0),
-    numero INTEGER CONSTRAINT ErroCartaoNumero CHECK (numero>0)
-);
-
-CREATE TABLE Prova (
-    quantidade INTEGER CONSTRAINT ErroProvaQuantidade CHECK (quantidade<=5),
-    idCliente INTEGER CONSTRAINT ErroProvaIdCliente REFERENCES Cliente (idPessoa) ON UPDATE CASCADE ON DELETE CASCADE,
-    idVinho INTEGER CONSTRAINT ErroProvaIdVinho REFERENCES Vinho (idProduto) ON UPDATE CASCADE ON DELETE CASCADE,
-    PRIMARY KEY (idCliente, idVinho)
 );
 
 CREATE TABLE Utensilio (
@@ -114,3 +92,35 @@ CREATE TABLE Vinho (
     capacidadeGarrafa FLOAT CONSTRAINT ErroVinhoCapacidadeGarrafa NOT NULL, 
     UNIQUE (tipo, regiao, anoProducao, teorAlcool, capacidadeGarrafa)
 );
+
+CREATE TABLE Cartao (
+    idCartao INTEGER PRIMARY KEY,
+    dataAdesao DATE CONSTRAINT ErroCartaoDataAdesao NOT NULL,
+    saldo INTEGER CONSTRAINT ErroCartaoSaldo CHECK (saldo>=0),
+    numero INTEGER CONSTRAINT ErroCartaoNumero CHECK (numero>0)
+);
+
+CREATE TABLE Prova (
+    quantidade INTEGER CONSTRAINT ErroProvaQuantidade CHECK (quantidade<=5),
+    idCliente INTEGER CONSTRAINT ErroProvaIdCliente REFERENCES Cliente (idPessoa) ON UPDATE CASCADE ON DELETE CASCADE,
+    idVinho INTEGER CONSTRAINT ErroProvaIdVinho REFERENCES Vinho (idProduto) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (idCliente, idVinho)
+);
+
+CREATE TABLE Stock (
+    idArmazem INTEGER CONSTRAINT ErroStockIdArmazem REFERENCES Armazem (idArmazem) ON DELETE CASCADE ON UPDATE CASCADE,
+    idProduto INTEGER CONSTRAINT ErroStockIdProduto REFERENCES Produto (idProduto) ON DELETE CASCADE ON UPDATE CASCADE,
+    quantidade INTEGER CONSTRAINT ErroStockQuantidade CHECK (quantidade > 0),
+    PRIMARY KEY (idArmazem, idProduto)
+);
+
+CREATE TABLE Compra (
+    idProduto INTEGER CONSTRAINT ErroCompraIdProduto REFERENCES Produto (idProduto) ON UPDATE CASCADE ON DELETE CASCADE,
+    idCliente INTEGER CONSTRAINT ErroCompraIdCliente REFERENCES Cliente (idPessoa) ON UPDATE CASCADE ON DELETE CASCADE,
+    data DATE CONSTRAINT ErroCompraData NOT NULL,
+    preco INTEGER CONSTRAINT ErroCompraPreco CHECK (preco>=0),
+    PRIMARY KEY (idCliente, idProduto)
+);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = ON;
