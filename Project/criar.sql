c9700abb1d9b070825c7088b494f87bd78cc77b2
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 16:49:00 +0000

    Something

--- Project/criar.sql
+++ Project/criar.sql
@@ -1,17 +1,69 @@
 -- Eliminar as tabelas, se existirem
 
+DROP TABLE IF EXISTS Pessoa;
+DROP TABLE IF EXISTS Armazem;
+DROP TABLE IF EXISTS Stock;
+DROP TABLE IF EXISTS Horario;
+DROP TABLE IF EXISTS HorarioFuncionario;
 DROP TABLE IF EXISTS Funcionario;
 DROP TABLE IF EXISTS Cliente;
 DROP TABLE IF EXISTS Produto;
 DROP TABLE IF EXISTS Gerente;
 DROP TABLE IF EXISTS NaoGerente;
+DROP TABLE IF EXISTS Cartao;
+DROP TABLE IF EXISTS Prova;
+DROP TABLE IF EXISTS Utensilios;
+DROP TABLE IF EXISTS Vinho;
+DROP TABLE IF EXISTS Compra;
 
 -- Criar as novas tabelas, com restrições
 
+CREATE TABLE Pessoa (
+    idPessoa INTEGER PRIMARY KEY,
+    nome CHAR(30) NOT NULL,
+    dataNascimento DATE NOT NULL,
+    telefone INTEGER NOT NULL,
+    idade INTEGER NOT NULL,
+    nif INTEGER NOT NULL  
+);
+
+CREATE TABLE Armazem (
+    idArmazem PRIMARY KEY,
+    telefone INTEGER,
+    local TEXT,
+    UNIQUE(telefone, local)
+);
+
+CREATE TABLE Stock (
+    idArmazem INTEGER REFERENCES Armazem (idArmazem) ON DELETE CASCADE ON UPDATE CASCADE,
+    idProduto INTEGER REFERENCES Produto (idProduto) ON DELETE CASCADE ON UPDATE CASCADE,
+    quantidade INTEGER CHECK (quantidade > 0),
+    PRIMARY KEY (idArmazem, idProduto)
+);
+
+CREATE TABLE Horario (
+    idHorario INTEGER PRIMARY KEY,
+    horaInicio TIME NOT NULL,
+    horaFinal TIME NOT NULL,
+    diaSemana TEXT CHECK ( (diaSemana = "SEGUNDA-FEIRA" OR 
+                            diaSemana = "TERCA-FEIRA" OR 
+                            diaSemana = "QUARTA-FEIRA" OR 
+                            diaSemana = "QUINTA-FEIRA" OR 
+                            diaSemana = "SEXTA-FEIRA" OR 
+                            diaSemana = "SABADO" OR 
+                            diaSemana = "DOMINGO" ) ),
+    UNIQUE (diaSemana, horaInicio, horaFinal)
+);
+
+CREATE TABLE HorarioFuncionario (
+    idFuncionario REFERENCES Funcionario (idFuncionario) ON UPDATE CASCADE ON DELETE CASCADE,
+    idHorario REFERENCES Horario (idHorario) ON UPDATE CASCADE ON DELETE CASCADE,
+    PRIMARY KEY (idFuncionario, idHorario)
+);
+
 CREATE TABLE Funcionario (
-    idPessoa INTEGER CONSTRAINT idPessoaPK PRIMARY KEY REFERENCES Pessoa(idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
-    salario INTEGER CONSTRAINT salarioNotNegative CHECK (salario >= 0),
-    numId INTEGER CONSTRAINT numIdNotNull NOT NULL
+    idPessoa PRIMARY KEY REFERENCES Pessoa (idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
+    salario CHECK (salario >= 0)
 );
 
 CREATE TABLE Cliente (
