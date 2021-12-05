e5bf7a5f11b21403e496a61d723f16f283df7492
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 23:32:14 +0000

    Added tables

--- Project/povoar.sql
+++ Project/povoar.sql
@@ -1,20 +1,20 @@
 PRAGMA foreign_keys = ON;
 BEGIN TRANSACTION;
 
--- Table 
--- Table
--- Table
--- Table
--- Table
--- Table 
--- Table
--- Table
--- Table
--- Table
--- Table 
--- Table
--- Table
--- Table
--- Table
+-- Table Pessoa
+-- Table Cartao
+-- Table Cliente
+-- Table Funcionario
+-- Table NaoGerente
+-- Table Gerente
+-- Table Produto
+-- Table Vinho
+-- Table Utensilio
+-- Table Horario
+-- Table Armazem
+-- Table Stock
+-- Table HorarioFuncionario
+-- Table Prova
+-- Table Compra
 
 COMMIT TRANSACTION;
