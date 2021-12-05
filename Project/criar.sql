2fdb90da2ba60dcc51cf8f379cad4283412342f1
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 15:32:39 +0000

    Drafts

--- Project/criar.sql
+++ Project/criar.sql
@@ -9,8 +9,26 @@ DROP TABLE IF EXISTS NaoGerente;
 -- Criar as novas tabelas, com restrições
 
 CREATE TABLE Funcionario (
-    idPessoa CONSTRAINT idPessoaPK PRIMARY KEY REFERENCES Pessoa(idPessoa),
-    salario INTEGER CONSTRAINT salarioNotNegative CHECK (salario >= 0)
+    idPessoa INTEGER CONSTRAINT idPessoaPK PRIMARY KEY REFERENCES Pessoa(idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
+    salario INTEGER CONSTRAINT salarioNotNegative CHECK (salario >= 0),
     numId INTEGER CONSTRAINT numIdNotNull NOT NULL
 );
 
+CREATE TABLE Cliente (
+    idPessoa INTEGER CONSTRAINT idPessoaPK PRIMARY KEY REFERENCES Pessoa(idPessoa) ON DELETE CASCADE ON UPDATE CASCADE,
+    maioridade CONSTRAINT maioridadeNotNull NOT NULL
+);
+
+CREATE TABLE Produto (
+    idProduto CONSTRAINT idProdutoPK PRIMARY KEY ON DELETE SET NULL ON UPDATE CASCADE,
+    preco CONSTRAINT precoNotNegative CHECK (preco >= 0)
+);
+
+CREATE TABLE Gerente (
+    idFuncionario CONSTRAINT idFuncionarioPK PRIMARY KEY REFERENCES Funcionario (idFuncionario) ON DELETE CASCADE ON UPDATE CASCADE,
+    anosServico CONSTRAINT anosServicoSupQuatro CHECK (anosServico > 4)
+);
+
+CREATE TABLE NaoGerente (
+    idFuncionario CONSTRAINT idFuncionarioPK PRIMARY KEY REFERENCES Funcionario (idFuncionario) ON UPDATE CASCADE ON DELETE CASCADE
+);
