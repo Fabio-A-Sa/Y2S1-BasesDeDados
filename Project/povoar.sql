efb4f8e66bc939bbf24bf32cfe6cdc18955f2a02
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 23:29:36 +0000

    Added table configs

--- Project/povoar.sql
+++ Project/povoar.sql
@@ -1,5 +1,20 @@
 PRAGMA foreign_keys = on;
 BEGIN TRANSACTION;
 
+-- Table 
+-- Table
+-- Table
+-- Table
+-- Table
+-- Table 
+-- Table
+-- Table
+-- Table
+-- Table
+-- Table 
+-- Table
+-- Table
+-- Table
+-- Table
 
 COMMIT TRANSACTION;
