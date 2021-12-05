07f671fbb26552089a3062eedb16f9d39e3d8cdc
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 14:59:30 +0000

    Begin

---
+++ Project/criar.sql
@@ -0,0 +1,13 @@
+-- Eliminar as tabelas, se existirem
+
+DROP TABLE IF EXISTS Funcionario;
+DROP TABLE IF EXISTS Cliente;
+DROP TABLE IF EXISTS Produto;
+DROP TABLE IF EXISTS Gerente;
+DROP TABLE IF EXISTS NaoGerente;
+
+-- Criar as novas tabelas, com restrições
+
+CREATE TABLE Funcionario (
+    
+)
