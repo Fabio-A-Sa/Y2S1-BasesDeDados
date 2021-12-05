e30b41299a7dcd58acc6ad505e5e21606f0fd23d
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-05 14:07:26 +0000

    Delete file

--- Notes/Run.sh
+++ Notes/Run.sh
@@ -1,3 +1,3 @@
 alias runSQL="winpty D:\\Programas\\SQLite\\sqlite3.exe"
-winpty D:\\Programas\\SQLite\\sqlite3.exe teste.db
+winpty D:\\Programas\\SQLite\\sqlite3.exe Notes.db
 open "1 - Tables.sql"
