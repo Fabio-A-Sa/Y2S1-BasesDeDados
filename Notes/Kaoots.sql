c5bb3567c57b1bcb6bf3f044370347763b76294f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-06 18:39:50 +0000

    Parte dos dados

--- Notes/Kaoots.sql
+++ Notes/Kaoots.sql
@@ -1,6 +1,9 @@
 PRAGMA foreign_keys = OFF;
 BEGIN TRANSACTION;
 
+.mode columns
+.headers on
+
 DROP TABLE IF EXISTS Kaoots;
 
 CREATE TABLE Kaoots (
@@ -11,11 +14,11 @@ CREATE TABLE Kaoots (
     CHECK (Acertadas <= Total)
 );
 
-INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
-INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
-INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
-INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
-INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
+INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-04", 9, 15, 5993);
+INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-08", 3, 4, 2127);
+INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-11", 4, 4, 3446);
+INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-15", 3, 4, 2212);
+INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-18", 4, 4, 3242);
 INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
 INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
 INSERT INTO Kaoots (Data, Acertadas, Total, Pontos) VALUES ("2021-11-", , , );
