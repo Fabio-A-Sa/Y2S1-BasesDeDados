e100b5c70d61f6777b7cd648ce21addc64b02d0b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-06 18:34:51 +0000

    Create table kaoots

--- Notes/Kaoots.sql
+++ Notes/Kaoots.sql
@@ -1,5 +1,14 @@
+PRAGMA foreign_keys = OFF;
+BEGIN TRANSACTION;
+
 DROP TABLE IF EXISTS Kaoots;
 
 CREATE TABLE Kaoots (
-    
+    Data date,
+    Acertadas INTEGER,
+    Total INTEGER,
+    Pontos INTEGER,
+    CHECK (Acertadas <= Total)
 );
+
+COMMIT TRANSACTION;
