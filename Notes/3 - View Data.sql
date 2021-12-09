02fcf837220f8ed3708f92db9c1a17d7992403ba
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-09 21:17:11 +0000

    Topic 3

--- Notes/3 - View Data.sql
+++ Notes/3 - View Data.sql
@@ -9,4 +9,4 @@
 -- To view all atributes of a table (continuation in topic 7)
 
 SELECT * FROM Person;                                    -- Show all atributes and tuples from current table Person
-SELECT name, date('2021-11-30')-birthday from Person;    -- Show name | age in current table Person
+SELECT name, date('2021-11-30')-birthday as age from Person;    -- Show name | age in current table Person
