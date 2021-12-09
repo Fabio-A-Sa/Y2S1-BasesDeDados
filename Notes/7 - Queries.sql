dc77047b349ac6891a380754cf507c9b31066acf
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-09 21:43:59 +0000

    A simple query

--- Notes/7 - Queries.sql
+++ Notes/7 - Queries.sql
@@ -1,5 +1,11 @@
--- a simple query
+-- A simple query
 
 SELECT * FROM Person;                               -- Show all data in Person's table
 SELECT name, birthDate-now as age FROM Person;      -- Show name | age in all tuples
 
+-- With conditions
+
+SELECT name FROM Person WHERE gender = 'M';                 -- Select all male persons
+SELECT name, address FROM Person WHERE birthDate-now < 20;  -- Select all persons which age < 20
+
+-- Sorting
