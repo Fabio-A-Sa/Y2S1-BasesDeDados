abeff6fa88dec38de91a10b2a40477651f3a7f25
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-09 21:40:52 +0000

    A simple query

--- Notes/7 - Queries.sql
+++ Notes/7 - Queries.sql
@@ -1,4 +1,5 @@
 -- a simple query
 
-SELECT * FROM Person;
-SELECT 
+SELECT * FROM Person;                               -- Show all data in Person's table
+SELECT name, birthDate-now as age FROM Person;      -- Show name | age in all tuples
+
