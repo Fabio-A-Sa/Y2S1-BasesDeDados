32c5472477256d2100e0476ac8abf2cdb7b2cab7
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-09 21:37:02 +0000

    Topic 6

--- Notes/6 - Referencial Integrity.sql
+++ Notes/6 - Referencial Integrity.sql
@@ -15,13 +15,14 @@ CREATE TABLE College (
 CREATE TABLE Apply (
     sID INTEGER REFERENCES Student(sID) ON UPDATE CASCADE ON DELETE SET NULL,            -- Student's primary key
     cName CHAR(10) REFERENCES College(cName) ON UPDATE CASCADE ON DELETE SET NULL,       -- College's primary key
-    PRIMARY KEY (sID, cName)                        -- Aplly is a primary key too
+    PRIMARY KEY (sID, cName)                                                             -- Apply is a primary key too
 )
 
 -- Note
 -- if Student has (sID, sName) for Primary Key, Apply will have
--- FOREING KEY (sID, sName) REFERENCES Student(sID, sName);
+-- FOREING KEY (sID, sName) REFERENCES Student (sID, sName);
 
 -- Note
 -- if Apply's sID is updated, Student will be updated too
 -- if Apply's cName is deleted, College will be set to NULL
+-- for default, SQL will throw an error
