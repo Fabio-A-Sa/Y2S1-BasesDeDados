dd68054a3d015c7a2a7ff83ae153ce0328354d8c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 21:57:00 +0000

    determine maximum valur of any atribute

--- Notes/8 - Subqueries.sql
+++ Notes/8 - Subqueries.sql
@@ -1,18 +1,17 @@
 -- A simple subquerie
 
-SELECT name FROM Student WHERE id IN            -- Returns student's name that are now enrolled in BD
-                        (SELECT id from Apply WHERE major = 'BD');
+SELECT name FROM Student WHERE id IN (
+    SELECT id from Apply WHERE major = 'BD'          -- Returns student's name that are now enrolled in BD
+);
 
 -- Exists operator
 
-SELECT name FROM Student WHERE id EXISTS IN    -- Returns student's name that has degree more than 16.0
-                         (SELECT id from Student WHERE degree > 16.0)
+SELECT name FROM Student WHERE id EXISTS IN (
+    SELECT id from Student WHERE GPA > 16.0       -- Returns student's name that has GPA more than 16.0
+);
 
 -- Maximum value
 
 SELECT name FROM Student S1 WHERE not exists (
-    SELECT * FROM Student S2 WHERE S2.GPA > S1.GPA
+    SELECT * FROM Student S2 WHERE S2.GPA > S1.GPA   -- Returns all students that have the same maximum GPA
 );
-
-     -- Returns student's name that are now enrolled in BD
-      -- Returns student's name that has degree more than 16.0
