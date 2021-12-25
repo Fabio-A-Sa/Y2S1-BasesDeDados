aa110392d383428be6999f9f6f20423b252c93e9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 23:25:15 +0000

    left outer join

--- Notes/9 - Joins.sql
+++ Notes/9 - Joins.sql
@@ -11,3 +11,8 @@ FROM Student JOIN Apply USING (sID)           -- Desde que se selecione um atrib
 
 SELECT DISTINCT sName, major                  -- Desde que tenham um atributo em comum, para fazer a devida junção
 FROM Student NATURAL JOIN Apply;
+
+-- Left outer join
+
+SELECT sName, sID, cName, major                     -- Todos os estudantes são ligados aos applies, e se não houver 
+FROM Student LEFT OUTER JOIN Apply using (sID)      -- ligação, cName e major ficam NULL
