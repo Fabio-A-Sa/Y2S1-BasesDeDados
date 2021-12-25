d519f16aa31b1ea4a914945450dc29d328232bf3
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 21:53:46 +0000

    drfat

--- Notes/8 - Subqueries.sql
+++ Notes/8 - Subqueries.sql
@@ -1,11 +1,18 @@
 -- A simple subquerie
 
 SELECT name FROM Student WHERE id IN            -- Returns student's name that are now enrolled in BD
-                        (SELECT id from Apply WHERE major = 'BD')
+                        (SELECT id from Apply WHERE major = 'BD');
 
 -- Exists operator
 
-SELECT name FROM Students WHERE id EXISTS IN    -- Returns student's name that has degree more than 16.0
+SELECT name FROM Student WHERE id EXISTS IN    -- Returns student's name that has degree more than 16.0
                          (SELECT id from Student WHERE degree > 16.0)
 
-                        
+-- Maximum value
+
+SELECT name FROM Student S1 WHERE not exists (
+    SELECT * FROM Student S2 WHERE S2.GPA > S1.GPA
+);
+
+     -- Returns student's name that are now enrolled in BD
+      -- Returns student's name that has degree more than 16.0
