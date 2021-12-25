c3a887459b4cdf6499d080df00fc7dfec243de1f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 23:36:43 +0000

    drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -256,3 +256,32 @@ SELECT DISTINCT name FROM Apply, Student WHERE Apply.SID = Student.SID AND GPA >
 
 SELECT * FROM (SELECT id, Name, GPA, GPA*(HS/1000) AS scaledGPA FROM Student) G
          WHERE abs(scaledGPA - GPA) > 1.0;
+
+-- Inner Join / Join for default
+
+SELECT sName, GPA 
+FROM Student JOIN Apply ON Student.sID = Apply.sID 
+WHERE HS < 1000 AND major = 'CS' AND cNAME = 'Stanford';
+
+SELECT DISTINCT nName, major
+FROM Student JOIN Apply USING (sID)           -- Desde que se selecione um atributo em comum nas duas tabelas
+
+-- Natural Join
+
+SELECT DISTINCT sName, major                  -- Desde que tenham um atributo em comum, para fazer a devida junção
+FROM Student NATURAL JOIN Apply;
+
+-- Left outer join / left join
+
+SELECT sName, sID, cName, major                      -- Todos os estudantes são ligados aos applies, e se não houver 
+FROM Student LEFT OUTER JOIN Apply using (sID)       -- ligação, cName e major ficam NULL
+
+-- Right outer join / right join
+
+SELECT sName, sID, cName, major                      -- Todos os applies são ligados aos estudantes, e se não houver 
+FROM Student RIGHT OUTER JOIN Apply using (sID)      -- ligação, sName e sID ficam NULL
+
+-- Full outer join / full join
+
+SELECT sName, sID, cName, major                      -- Todos os applies são ligados aos estudantes e todos os estudantes, e se não houver 
+FROM Student FULL OUTER JOIN Apply using (sID)       -- ligados aos applies, se não houver ligação ligação, ficam a NULL
