951a7ebd2ab9eba867bc590b3c7c1c0f6cb70a67
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 21:38:05 +0000

    more details

--- Notes/8 - Subqueries.sql
+++ Notes/8 - Subqueries.sql
@@ -1,5 +1,9 @@
 -- A simple subquerie
 
-SELECT name FROM student WHERE id IN 
-                                  (SELECT id from Apply WHERE major = 'BD')
+SELECT name FROM Student WHERE id IN            -- Returns student's name that are now enrolled in BD
+                        (SELECT id from Apply WHERE major = 'BD')
 
+-- Exists operator
+
+SELECT name FROM Students WHERE id EXISTS IN 
+                         (SELECT id from Student WHERE )
