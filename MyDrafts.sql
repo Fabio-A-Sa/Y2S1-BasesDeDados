92ee62416fef5e69418547380a136121f35acac8
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 21:58:51 +0000

    drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -216,3 +216,21 @@ SELECT student.ID from Student WHERE major = 'BD'
 SELECT student.ID from Student WHERE major = 'AED'
 EXCEPT                                                  -- Select all students that are enrolled in AED and not in BD
 SELECT student.ID from Studnt WHERE major = 'BD'
+
+-- A simple subquerie
+
+SELECT name FROM Student WHERE id IN (
+    SELECT id from Apply WHERE major = 'BD'          -- Returns student's name that are now enrolled in BD
+);
+
+-- Exists operator
+
+SELECT name FROM Student WHERE id EXISTS IN (
+    SELECT id from Student WHERE GPA > 16.0       -- Returns student's name that has GPA more than 16.0
+);
+
+-- Maximum value
+
+SELECT name FROM Student S1 WHERE not exists (
+    SELECT * FROM Student S2 WHERE S2.GPA > S1.GPA   -- Returns all students that have the same maximum GPA
+);
