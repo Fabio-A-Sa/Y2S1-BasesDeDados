d8c51f8829b95877c1bcc15ec1a2d5e8c4e71422
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-25 23:06:31 +0000

    natural join

--- Notes/9 - Joins.sql
+++ Notes/9 - Joins.sql
@@ -1,3 +1,10 @@
 -- Inner Join / Join for default
 
-SELECT sName, GPA FROM Student JOIN Apply ON Student.sID = Apply.sID WHERE HS < 1000 AND major = 'CS' AND cNAME = 'Stanford';
+SELECT sName, GPA 
+FROM Student JOIN Apply ON Student.sID = Apply.sID 
+WHERE HS < 1000 AND major = 'CS' AND cNAME = 'Stanford';
+
+-- Natural Join
+
+SELECT DISTINCT sName, major                -- Desde que tenham um atributo em comum, para fazer a devida junção
+FROM Student NATURAL JOIN Apply;
