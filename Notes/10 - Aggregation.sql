ca5f3acee5fad6add5491eaeecdb9209634d56cd
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-24 17:53:18 +0000

    some notes

--- Notes/10 - Aggregation.sql
+++ Notes/10 - Aggregation.sql
@@ -1,3 +1,19 @@
 -- Created on January, 2022
 -- @author: Fábio Araújo de Sá
 
+SELECT min(GPA)
+FROM Student
+WHERE Student.sID = Apply.sID AND major = 'CS';
+
+SELECT max(abs(maximum - minimum)) as difference
+FROM (
+    SELECT cName, major, min(GPA) as minimum, max(GPA) as maximum
+    FROM Student, Apply
+    WHERE Student.id = Apply.id
+    GROUP BY cName, major
+);
+
+SELECT cName
+FROM Apply
+GROUP BY cName
+HAVING count(DISTINCT sID) < 5;
