03e3356ed18741dbf78410bf8d8c1a5c798121dc
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-24 18:25:47 +0000

    new file

--- Notes/11 - Modification.sql
+++ Notes/11 - Modification.sql
@@ -1,3 +1,34 @@
 -- Created on January, 2022
 -- @author: Fábio Araújo de Sá
 
+-- Inserir, atualizar e eliminar
+
+INSERT INTO Student (name, college) VALUES ('Fabio', 'FEUP');
+
+INSERT INTO Apply
+SELECT sID, 'FEUP', 'Eng. Informática', NULL
+FROM Student
+WHERE sID NOT IN (
+    SELECT sID
+    FROM Apply
+);
+
+DELETE FROM Student
+WHERE Student.id IN (
+    SELECT sID 
+    FROM Apply
+    GROUP BY sID
+    HAVING count(DISTINCT major) > 2
+);
+
+UPDATE Apply
+SET desision = 'Y', major = 'Eng. Informática'
+WHERE cName = 'FEUP' AND sID in (
+    SELECT sID
+    FROM Student
+    WHERE GPA < 3.6
+);
+
+UPDATE Student
+SET GPA = (SELECT max(GPA) FROM Student), 
+    sizeHS = (SELECT min(sizeHS) FROM Student);
