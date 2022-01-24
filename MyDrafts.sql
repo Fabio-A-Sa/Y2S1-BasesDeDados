a2cdf48df300d729df69a1102c4678e7e354ef5b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-24 19:22:50 +0000

    Drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -1626,3 +1626,29 @@ WHERE cName = 'FEUP' AND sID in (
 UPDATE Student
 SET GPA = (SELECT max(GPA) FROM Student), 
     sizeHS = (SELECT min(sizeHS) FROM Student);
+
+CREATE TRIGGER example
+AFTER UPDATE OF cName ON college
+FOR EACH ROW
+BEGIN
+    UPDATE Apply
+    SET cName = NEW.cName
+    WHERE cName = OLD.cName;
+END;
+
+INSERT INTO CSEE VALUES (111, 'Berkeley', 'CS');
+
+CREATE TRIGGER CSEEinsert
+INSTEAD OF INSERT ON CSEE
+FOR EACH ROW
+BEGIN
+    INSERT INTO Apply
+    VALUES (NEW.sID, NEW.cName, NEW.major, NULL);
+END;
+
+CREATE INDEX courseIDX on Exam(course_code); --> cria um index, para melhorar a performance caso
+                                             --> o atributo de course_code for muito usado no sistema
+
+-- Notes:
+-- Index baseado em igualdade -> usa HashTables
+-- Index baseado em comparações -> usa B-Trees
