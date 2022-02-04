67f4b10fa977d53a8b217f102690b4a08e8c315f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-02-04 18:21:29 +0000

    5.2

--- Exercises/exam2016resolution.sql
+++ Exercises/exam2016resolution.sql
@@ -6,3 +6,22 @@ WHERE Estudante.curso = Curso.ID AND Estudante.anoCurricular = 3;
 
 -- 5.2
 
+SELECT Estudante.nome, Estudante.anoCurricular
+FROM Estudante
+WHERE Estudante.ID IN (
+	SELECT Estudante.ID FROM ( SELECT Estudante.ID, max(qtd) 
+	FROM (
+	SELECT Estudante.ID, count(Amizade.ID2) as qtd
+	FROM Estudante, Amizade
+	WHERE Estudante.ID = Amizade.ID1
+	GROUP BY Estudante.ID
+	))
+);
+
+SELECT Estudante.ID, max(qtd) 
+FROM (
+	SELECT Estudante.ID, count(Amizade.ID2) as qtd
+	FROM Estudante, Amizade
+	WHERE Estudante.ID = Amizade.ID1
+	GROUP BY Estudante.ID
+)
