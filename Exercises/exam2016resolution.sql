a02f390dd078e8f03ad786dec47feea264ea4276
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-02-04 18:50:38 +0000

    5.3

--- Exercises/exam2016resolution.sql
+++ Exercises/exam2016resolution.sql
@@ -9,19 +9,29 @@ WHERE Estudante.curso = Curso.ID AND Estudante.anoCurricular = 3;
 SELECT Estudante.nome, Estudante.anoCurricular
 FROM Estudante
 WHERE Estudante.ID IN (
-	SELECT Estudante.ID FROM ( SELECT Estudante.ID, max(qtd) 
-	FROM (
-	SELECT Estudante.ID, count(Amizade.ID2) as qtd
-	FROM Estudante, Amizade
-	WHERE Estudante.ID = Amizade.ID1
-	GROUP BY Estudante.ID
-	))
+    SELECT ID 
+    FROM (
+        SELECT ID, max(qtd) 
+        FROM (
+            SELECT Estudante.ID, count(Amizade.ID2) as qtd
+            FROM Estudante, Amizade
+            WHERE Estudante.ID = Amizade.ID1
+            GROUP BY Estudante.ID
+        )
+    )
 );
 
-SELECT Estudante.ID, max(qtd) 
-FROM (
-	SELECT Estudante.ID, count(Amizade.ID2) as qtd
-	FROM Estudante, Amizade
-	WHERE Estudante.ID = Amizade.ID1
-	GROUP BY Estudante.ID
-)
+-- 5.3
+
+CREATE TRIGGER Trigger1
+AFTER INSERT ON Estudante
+FOR EACH ROW
+BEGIN
+	INSERT INTO Amizade VALUES (id1, id2)
+	SELECT e1.ID as id1, e2.ID as id2
+	FROM Estudante AS e1, Estudate AS e2
+	WHERE 	(e1.ID = NEW.ID OR e2.ID = NEW.ID) AND
+		e1.curso = e2.curso AND
+		e1.ID != e2.ID;
+END
+
