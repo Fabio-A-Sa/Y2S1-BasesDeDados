7c1892463277f59691278693149c9d132ea5e489
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-02-05 12:12:32 +0000

    Drafts update

--- MyDrafts.sql
+++ MyDrafts.sql
@@ -2129,3 +2129,84 @@ insert into Amizade values (201101934, 201101304);
 insert into Amizade values (201101304, 201101661);
 insert into Amizade values (201101661, 201101025);
 insert into Amizade select ID2, ID1 from Amizade;
+
+-- 5.1
+
+SELECT Estudante.nome, Curso.nome
+FROM Estudante, Curso
+WHERE Estudante.curso = Curso.ID AND Estudante.anoCurricular = 3;
+
+-- 5.2
+
+SELECT Estudante.nome, Estudante.anoCurricular
+FROM Estudante
+WHERE Estudante.ID IN (
+    SELECT ID 
+    FROM (
+        SELECT ID, max(qtd) 
+        FROM (
+            SELECT Estudante.ID, count(Amizade.ID2) as qtd
+            FROM Estudante, Amizade
+            WHERE Estudante.ID = Amizade.ID1
+            GROUP BY Estudante.ID
+        )
+    )
+);
+
+-- 5.3
+
+CREATE TRIGGER Trigger1
+AFTER INSERT ON Estudante
+FOR EACH ROW
+BEGIN
+	INSERT INTO Amizade VALUES (id1, id2)
+	SELECT e1.ID as id1, e2.ID as id2
+	FROM Estudante AS e1, Estudate AS e2
+	WHERE 	(e1.ID = NEW.ID OR e2.ID = NEW.ID) AND
+		e1.curso = e2.curso AND
+		e1.ID != e2.ID;
+END
+
+-- 5.4
+
+SELECT e1.nome, e1.anoCurricular
+FROM Estudante AS e1
+WHERE EXISTS (
+	SELECT Amizade.ID2
+	FROM Estudante AS e2, Amizade
+	WHERE 	Amizade.ID1 = e1.ID AND
+		e2.ID = Amizade.ID2 AND 
+		e2.anoCurricular = e1.anoCurricular
+) AND NOT EXISTS (
+	SELECT Amizade.ID2
+	FROM Estudante AS e2, Amizade
+	WHERE 	Amizade.ID1 = e1.ID AND
+		e2.ID = Amizade.ID2 AND 
+		e2.anoCurricular != e1.anoCurricular
+)
+ORDER BY e1.anoCurricular, e1.nome;
+
+-- 5.5
+
+SELECT Estudante.nome
+FROM Estudante, Amizade
+WHERE Estudante.ID = Amizade.ID1
+GROUP BY Estudante.ID
+HAVING count(Amizade.ID2) > 3;
+
+-- 5.6
+
+SELECT DISTINCT ID2 AS ID 
+FROM AMIZADE
+WHERE ID1 IN (
+    SELECT ID2 AS ID
+    FROM AMIZADE WHERE ID1 IN (
+        SELECT ID2 AS ID 
+        FROM AMIZADE 
+        WHERE ID1 IN (
+            SELECT ID
+            FROM Estudante
+            WHERE nome LIKE 'Miguel Sampaio'
+        )
+    )
+);
